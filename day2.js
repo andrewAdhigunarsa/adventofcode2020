9ed0d47c696164ce2b23b396d96a8a6d8a0db300
Andrew Adhigunarsa <andrewadhigunarsa@192-168-1-105.tpgi.com.au>
2020-12-02 18:26:30 +1100

    day2

--- day2.js
+++ day2.js
@@ -43,6 +43,52 @@ function day2(list){
             return (numberOfOccurence <= item.maxOccurrence) && (numberOfOccurence >= item.minOccurrence);
         }
     )
-    console.log(processedList.length);    
+   
+    console.log(validPasswords.length);
+}
+
+function day2part2(list){
+    if(!list){
+        return;
+    }
+
+    const lines = list.split(/\n/);
+    let listArray = [];
+    let outputText = [];
+    for (let i = 0; i < lines.length; i++) {
+        // only push this line if it contains a non whitespace character.
+        if (/\S/.test(lines[i])) {
+        outputText.push('"' + lines[i].trim() + '"');
+        listArray.push(lines[i].trim());
+        }
+    }
+    
+    const processedList = listArray.map(
+        (item)=>{
+           const itemArray = item.split(' ');
+           const policy = itemArray[0].split('-');
+           const firstPositionIndex = policy[0]-1;
+           const secondPositionIndex = policy[1]-1;
+           const keyLetter = itemArray[1];
+           const password = itemArray[2];
+           return{
+            firstPositionIndex,
+            secondPositionIndex,
+            keyLetter,
+            password
+           }   
+        }
+    )
+
+    const validPasswords = processedList.filter(
+        (item)=>{
+            const keyLetter = item.keyLetter.split(':')[0];
+            const letterArray = item.password.split('');
+            const firstOccurence = letterArray[item.firstPositionIndex]===keyLetter;
+            const secondOccurence = letterArray[item.secondPositionIndex]===keyLetter;
+            return !(firstOccurence === secondOccurence)
+        }
+    )
+   
     console.log(validPasswords.length);
 }
