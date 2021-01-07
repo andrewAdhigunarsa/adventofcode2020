78f73e3c17b8fe81c280dc004e0c77e94aa0cf07
Andrew Adhigunarsa <andrewadhigunarsa@192-168-1-105.tpgi.com.au>
2021-01-07 13:26:16 +1100

    formatted

--- day13/day13q.js
+++ day13/day13q.js
@@ -1,69 +1,57 @@
-var fs = require('fs');
+var fs = require("fs");
 
-function day13(data){
-    if(!data){
-        return;
-    }
+function day13(data) {
+	if (!data) {
+		return;
+	}
 
-    function processedString(value, regex){
-        const lines = value.split(regex);
-        let listArray = [];
-        let outputText = [];
-        for (let i = 0; i < lines.length; i++) {
-            // only push this line if it contains a non whitespace character.
-            if (/\S/.test(lines[i])) {
-            outputText.push('"' + lines[i].trim() + '"');
-            listArray.push(lines[i].trim());
-            }
-        }
+	function processedString(value, regex) {
+		const lines = value.split(regex);
+		let listArray = [];
+		let outputText = [];
+		for (let i = 0; i < lines.length; i++) {
+			// only push this line if it contains a non whitespace character.
+			if (/\S/.test(lines[i])) {
+				outputText.push('"' + lines[i].trim() + '"');
+				listArray.push(lines[i].trim());
+			}
+		}
 
-        return listArray;
-    }
+		return listArray;
+	}
 
-    const rawData = processedString(data, /\n/);
+	const rawData = processedString(data, /\n/);
 
-    let x = rawData[1].split(',');
+	let x = rawData[1].split(",");
 
-    let busesData  = x.map((e)=>{
-        if(e==='x'){
-            return '0';
-        }
-        return e;
-    }); 
+	let busesData = x.map((e) => {
+		if (e === "x") {
+			return "0";
+		}
+		return e;
+	});
 
-    const buses = busesData.map(e=> parseInt(e))
+	const buses = busesData.map((e) => parseInt(e));
 
-    let busesCopy = buses.map(e=>e);
+	let busesCopy = buses.map((e) => e);
 
-    let timeStamp = 0;
-    let w = 1;
+	let timeStamp = 0;
+	let w = 1;
 
-    busesCopy.forEach((item, index) => {
-        if(item===0){
+	busesCopy.forEach((item, index) => {
+		if (item === 0) {
+		} else {
+			for (let a = index; (timeStamp + a) % item !== 0; timeStamp += w) {}
+			w *= item;
+		}
+	});
 
-        }else{ 
-            for(let a=index; (timeStamp+a)%item!==0;timeStamp+=w){
-
-            }
-            w*=item;
-        }
-            
-    });
-
-
-    console.log(busesCopy)    
-    console.log(timeStamp) 
-
-    console.log(7%7);
- 1
+	console.log(busesCopy);
+	console.log(timeStamp);
 }
 
+fs.readFile("./data.js", "utf8", function (err, data) {
+	if (err) throw err;
 
-
-
-
-fs.readFile('./data.js','utf8', function (err, data) {
-  if (err) throw err;
-  
-  console.log(day13(data));
+	console.log(day13(data));
 });
